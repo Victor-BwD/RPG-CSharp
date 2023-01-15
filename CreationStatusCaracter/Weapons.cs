7155fd2fbd2761600c1f48c30537fc0563c29709
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-01-15 00:54:22 -0300

    Weapons classes and interface

---
+++ CreationStatusCaracter/Weapons.cs
@@ -0,0 +1,71 @@
+namespace CreationStatusCaracter;
+
+public interface Weapons
+{
+    string WeaponName { get; }
+    
+    int Damage { get; }
+
+    int StatusMultiplier();
+
+    static Weapons? Equip(string name)
+    {
+        if (ReferenceEquals(name, null)) return null;
+        return name.Trim().ToLower() switch
+        {
+            "sword" => new Sword(),
+            "greatsword" => new Greatsword(),
+            "dagger" => new Dagger(),
+            "staff" => new Staff(),
+            _ => null
+        };
+    }
+}
+
+public class Sword : Weapons
+{
+    public string WeaponName => "Sword";
+
+    public int Damage => 5;
+    
+    public int StatusMultiplier()
+    {
+        return 3;
+    }
+}
+
+public class Greatsword : Weapons
+{
+    public string WeaponName => "Greatsword";
+
+    public int Damage => 10;
+    
+    public int StatusMultiplier()
+    {
+        return 5;
+    }
+}
+
+public class Dagger : Weapons
+{
+    public string WeaponName => "Dagger";
+
+    public int Damage => 3;
+    
+    public int StatusMultiplier()
+    {
+        return 2;
+    }
+}
+
+public class Staff : Weapons
+{
+    public string WeaponName => "Staff";
+
+    public int Damage => 1;
+    
+    public int StatusMultiplier()
+    {
+        return 1;
+    }
+}
