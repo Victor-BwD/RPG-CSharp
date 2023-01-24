84aeb4dc6b0c952296a68dfb9e1cdee142e28e66
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-01-24 16:02:57 -0300

    Forest with goblins part created

--- CreationStatusCaracter/TheGame.cs
+++ CreationStatusCaracter/TheGame.cs
@@ -22,6 +22,17 @@ public class FirstEncouter
             throw new ArgumentOutOfRangeException("value", ("Oh, it's a shame... good bye."));
         }
 
-        
+        Console.WriteLine("You leave the tavern, holding a piece of paper the old man gave to you. In it is a coordinate for a dungeon, it says that it contains a lost sacred tome that belonged to the old master of this strange person.");
+        Console.WriteLine($"You are a experienced {playerCharacter.JobName.ToLower()}, you recognize the region, you know that to go to this dungeon you will have to face a not very inviting forest.");
+        Console.WriteLine("You're walking through the forest, but you hear thin voices in different languages, something you've heard before... It's goblins, they're trying to ambush you.");
+        Console.WriteLine("You still have a few km to walk inside the forest. You need to roll the dice and roll a number greater than 11 so that you can hide from the goblins and not be attacked. Roll the dice now...");
+
+        Random forestDice = new Random();
+        int rndNumber = forestDice.Next(1, 20);
+        Console.WriteLine($"You roll {rndNumber}");
+        if (rndNumber < 11)
+        {
+            Console.WriteLine("They see you, a goblin jumps in front of you and attacks you!");
+        }
     }
 }
