76fdf575ab0c583e2c6d6e94c086cd877d608307
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2023-03-28 02:44:37 -0300

    XP method

--- GameRPG/PlayerCharacter.cs
+++ GameRPG/PlayerCharacter.cs
@@ -4,6 +4,7 @@ public class PlayerCharacter
 {
     private string _name;
     private Status _stats;
+    private int _xp;
     private readonly IJob _job;
 
     private static int _level;
@@ -28,6 +29,25 @@ public class PlayerCharacter
     public string DisplayCharacterInfo()
     {
         return $"Name: {_name}, strength: {_stats.GetStrength()}, dexterity: {_stats.GetDex()}, intelligence: {_stats.GetIntelligence()}, charisma: {_stats.GetCharisma()}. Your character is lvl {_level}.";
+    }
+
+    public string DisplayLevel()
+    {
+        return $"Level: {_level}";
+    }
+
+    public void IncreaseXP(int xp)
+    {
+        _xp += xp;
+        if (_level == 1 && _xp >= 200)
+        {
+            SetLevel(2);
+        }
+
+        if (_level == 2 && _xp >= 500)
+        {
+            SetLevel(3);
+        }
     }
 
     public static void SetLevel(int level)
